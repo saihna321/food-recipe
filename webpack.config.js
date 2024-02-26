26d6762c21a563d1659e4a7b932c44dd8b5ebe0c
saikhna <saikhanjargal.0321@gmail.com>
2024-02-26 13:41:06 +0800

    docs folder uusgev

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './src/js/index.js',
     output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/main.js',
+    path: path.resolve(__dirname, 'docs'),
     },
     devServer: {
-        contentBase: './dist',
+        contentBase: './docs',
     },
     plugins: [
         new HtmlWebpackPlugin({  // Also generate a test.html
